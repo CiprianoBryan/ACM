be20df5b7236cb1c28ce3f2c0608bea9ef122eca
Bryan Tony <ciprianot.bryan@gmail.com>
2018-06-15 11:31:18 -0500

    empty

--- Contest/A.cpp
+++ Contest/A.cpp
@@ -6,43 +6,8 @@ const int INF = 1e9 + 7 ;
 
 /************************************/
 
-int v[ N ] ;
-int t[ N ] ;
-long long T[ N ] ;
-
-int bs( int i , int n ) {
-	int l = i , r = n ;
-	while( l <= r ) {
-		int me = ( l + r ) >> 1 ;
-		if( T[ me ] - T[ i - 1 ] >= v[ i ] ) r = me - 1 ;
-		else l = me + 1 ;
-	}
-	return l ;
-}
-
-int cantt[ N ] ;
-long long add[ N ] ;
-
 int main() {
-	int n ;
-	cin >> n ;
-	for( int i = 1 ; i <= n ; i ++ ) {
-		scanf( "%d" , &v[ i ] ) ;
-	}
-	for( int i = 1 ; i <= n ; i ++ ) {
-		scanf( "%d" , &t[ i ] ) ;
-		T[ i ] = T[ i - 1 ] + t[ i ] ;
-	}
-	for( int i = 1 ; i <= n ; i ++ ) {
-		int r = bs( i , n ) ;
-		add[ r ] += v[ i ] - ( T[ r - 1 ] - T[ i - 1 ] ) ;
-		cantt[ i ] ++ ;
-		cantt[ r ] -- ;
-	}
-	for( int i = 1 ; i <= n ; i ++ ) {
-		cantt[ i ] += cantt[ i - 1 ] ;
-		printf( "%lld " , add[ i ] + 1LL*t[ i ]*cantt[ i ] ) ;
-	}
+	
 
     return 0 ;
 }
